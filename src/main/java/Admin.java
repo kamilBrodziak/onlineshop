9de762b24271030a7de82d76c2510b7de7f53fd1
Kamil Brodziak <kamil98brodziak@gmail.com>
2018-11-12 11:30:49 +0100

    Start admin and customer

---
+++ src/main/java/Admin.java
@@ -0,0 +1,5 @@
+public class Admin extends User {
+    public Admin(int id, String login, String password, ProductController productController) {
+        super(id, login, password, productController);
+    }
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,74 +5,13 @@ public class User {
     private int id;
     private String login;
     private String password;
-    ProductController productController;
+    protected ProductController productController;
 
     public User(int id, String login, String password, ProductController productController) {
         this.id = id;
         this.login = login;
         this.password = password;
         this.productController = productController;
-    }
-
-    public void login() {
-        Scanner scanner = new Scanner(System.in);
-        String choice = "0";
-        while(choice != "0") {
-            System.out.println("1.Show products\t2.Create new order\t0.Quit");
-            if(scanner.hasNextLine()) {
-                choice = scanner.nextLine();
-            }
-            switch (choice) {
-                case "1":
-                    break;
-                case "2":
-                    createNewOrder();
-                    break;
-                case "0":
-                    System.out.println("Logout");
-                    return;
-                default:
-                    System.out.println("No such option.");
-            }
-        }
-    }
-
-    public void createNewOrder() {
-        Order order = new Order();
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("New order creator.");
-
-        String quitOption = "0";
-        String choose = "";
-
-        while(!choose.equals(quitOption) && !choose.equals("1")) {
-            System.out.println("1.Pay\n2.Show basket\n3.Add product\n4.Delete product\n0.Quit");
-            if(scanner.hasNextLine()) {
-                choose = scanner.nextLine();
-            }
-
-            switch(choose) {
-                case "1":
-                    order.pay();
-                    break;
-                case "2":
-                    viewBasket(order);
-                    break;
-                case "3":
-                    order.addToBasket(productController.getProduct());
-                case "4":
-                    deleteProduct(order);
-                    break;
-                case "0":
-                    return;
-                default:
-                    System.out.println("No such option.");
-            }
-
-        }
-
     }
 
     private void viewBasket(Order order) {
