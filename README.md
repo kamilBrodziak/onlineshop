adc7717ba643295d4a07fcd984a32287006b2cf9
Kamil Brodziak <kamil98brodziak@gmail.com>
2018-11-12 11:59:04 +0100

    Add maven exec

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 how to run:
 java -classpath ".:sqlite-jdbc-3.23.1.jar" Main
+
+mvn exec:java -Dexec.mainClass="Main"
--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Admin extends User {
     public Admin(int id, String login, String password, ProductController productController) {
         super(id, login, password, productController);
+    }
+
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        String choice = "0";
+        while(choice != "0") {
+            System.out.println("1.Show products\t2.Create new order\t0.Quit");
+            if(scanner.hasNextLine()) {
+                choice = scanner.nextLine();
+            }
+            switch (choice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "0":
+                    System.out.println("Logout");
+                    return;
+                default:
+                    System.out.println("No such option.");
+            }
+        }
     }
 }
