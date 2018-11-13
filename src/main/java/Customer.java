603119605ede536dddbfc125086c0da0e0f2d08b
Kamil <kamil.m.olsza@gmail.com>
2018-11-13 10:53:30 +0100

    Merge conflicts

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -61,11 +61,21 @@ public class Customer extends User {
         System.out.println("Your basket:");
         Iterator iterator = order.getBasketIterator();
         int i = 0;
-        System.out.println("id\t\tname\t\tamount");
+//        System.out.println("id\t\tname\t\tamount");
+//        while( iterator.hasNext()) {
+//            Product product = (Product)iterator.next();
+//            System.out.println(++i + "\t\t" + product.getName() + "\t\t" + order.getBasket().getAmount(product));
+//        }
+        String leftAlignFormat = "|  %-4d | %-20s |  %-6d  |%n";
+
+        System.out.format("+-------+----------------------+----------+%n");
+        System.out.format("|  ID   |  Name                |  Amount  |%n");
+        System.out.format("+-------+----------------------+----------+%n");
         while( iterator.hasNext()) {
             Product product = (Product)iterator.next();
-            System.out.println(++i + "\t\t" + product.getName() + "\t\t" + order.getBasket().getAmount(product));
+            System.out.format(leftAlignFormat, ++i, product.getName(), order.getBasket().getAmount(product));
         }
+        System.out.format("+-------+----------------------+----------+%n");
     }
 
     private void addToBasket(Order order) {
